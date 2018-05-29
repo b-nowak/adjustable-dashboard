336ab37c0f34904f52adcf36ec04bd5d3790c1a5
Bartosz Nowak <bartosz.nowak@polcode.net>
2018-05-29 14:03:48 +0200

    Fix RWD

--- script/script.js
+++ script/script.js
@@ -2,34 +2,12 @@ var app = new Vue({
   el: '#pc-data-aggregator',
 
   data: {
+    isToggled: false,
     isChangedPrimary: false,
-    isHiddenPrimary: false,
+    isHiddenPrimary: true,
     isChangedSecondary: false,
-    isHiddenSecondary: false,
-    isHiddenFooter: false,
+    isHiddenSecondary: true,
+    isHiddenFooter: true,
   },
-
-  methods: {
-    changePrimaryHeaderPosition: function () {
-      this.isChangedPrimary = !this.isChangedPrimary;
-      console.log("changed")
-    },
-    hidePrimaryHeader: function () {
-      this.isHiddenPrimary = !this.isHiddenPrimary;
-      console.log("hidden")
-    },
-
-    changeSecondaryHeaderPosition: function () {
-      this.isChangedSecondary = !this.isChangedSecondary;
-      console.log("changed")
-    },
-    hideSecondaryHeader: function () {
-      this.isHiddenSecondary = !this.isHiddenSecondary;
-      console.log("hidden")
-    },
-    hideFooter: function () {
-      this.isHiddenFooter = !this.isHiddenFooter;
-      console.log("hidden")
-    }
-  }
+  methods: {}
 })
